ca959c9c3db4a3ef1586ad0fcc1015191dcc274d
jv-vogler <jvsvogler@gmail.com>
2025-08-10 16:50:01 -0300

    refactor: use zod to create schemas and types

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,23 @@
-export type Source = string;
-export type Version = `${number}.${number}.${number}`;
+import { z } from 'zod';
 
-export type Package = {
-  name: string;
-  version: Version;
-  source?: Source;
-};
+export const SourceSchema = z.string();
 
-export type Manifest = {
-  name: string;
-  version: Version;
-  dependencies: Record<Package['name'], Omit<Package, 'name'> | Package['version']>;
-  defaultSource?: Source;
-};
+export const VersionSchema = z.string().regex(/^\d+\.\d+\.\d+$/, 'Version must be in format x.y.z');
+
+export const PackageSchema = z.object({
+  name: z.string().min(1, 'Package name cannot be empty'),
+  version: VersionSchema,
+  source: SourceSchema.optional(),
+});
+
+export const ManifestSchema = z.object({
+  name: z.string().min(1, 'Manifest name cannot be empty'),
+  version: VersionSchema,
+  dependencies: z.record(z.string(), z.union([PackageSchema.omit({ name: true }), VersionSchema])),
+  defaultSource: SourceSchema.optional(),
+});
+
+export type Source = z.infer<typeof SourceSchema>;
+export type Version = z.infer<typeof VersionSchema>;
+export type Package = z.infer<typeof PackageSchema>;
+export type Manifest = z.infer<typeof ManifestSchema>;
